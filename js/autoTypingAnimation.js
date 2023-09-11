7f7fd202701ffa48af4e10cf9bd549b1a1fe93ad
Rahat Khan <109203384+rahatkhan100@users.noreply.github.com>
2023-09-11 13:13:36 +0600

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Rahat Khan.</span> I am a professional Web Designer and Web Devolaper with an experience of about 3+ years in this profession. , I am a Full stack devolaper . I specialize in working with html css js php to create website designs and other web-based solutions. I have been working with Web design for several years now, about 3 years, and have gained a lot of knowledge and experience in the process. I have worked on various  projects, from small blogging sites to big corporate websites, and also make clients happy by guiding them on how to operate online shops and websites, I feel it is my responsibility and 100% to make clients happy with my skills. Makes happy. I am well-versed in the WordPress platform and have a deep understanding of its various features and capabilities. I am comfortable working with any version of WordPress and am confident in my ability to customize and develop websites that meet any client's needs. If you need  help and want to work with a professional, I'm your person. Thank you for your time and I look forward to hearing from you and being at your service anytime "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
